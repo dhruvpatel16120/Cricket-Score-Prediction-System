5b68ca8d0027fdd91bee502c43c2bfa1d27504ee
Dhruv PATEL <143148503+dhruvpatel16120@users.noreply.github.com>
2024-12-08 13:56:13 +0530

    Update Cricket-Score-Prediction.py

--- Cricket-Score-Prediction.py
+++ Cricket-Score-Prediction.py
@@ -180,7 +180,7 @@ def main():
         }
         </style>
         <div class="footer">
-            Created By <a href="https://dhruvpatelofficial.vercel.app" target="_blank">Dhruv Patel, Ansh Patel, Shaimindrasinh</a>
+            Created By <a href="https://dhruvpatelofficial.vercel.app" target="_blank">Dhruv Patel</a>
         </div>
         """,
         unsafe_allow_html=True
